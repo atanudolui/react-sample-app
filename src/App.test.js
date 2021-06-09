c4276219e86f277e6ecce4a4d20e3deda4f2a050
Atanu Dolui <atanudolui@Atanus-MacBook-Pro.local>
2021-06-09 20:44:14 +0530

    Fix issue

--- src/App.test.js
+++ src/App.test.js
@@ -27,7 +27,7 @@ describe("RegistrationForm", () => {
         userEvent.clear(emailInput);
         userEvent.type(emailInput, "dolui.atanu@gmail.com");
 
-        const button = await screen.getByRole('button', { name: 'save'});
+        const button = await screen.getByRole('button');
         userEvent.click(button);
 
         const listItemElements = await screen.findAllByRole('listitem');
